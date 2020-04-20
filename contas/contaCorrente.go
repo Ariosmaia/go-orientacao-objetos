b6e0cacef0cc1cab9b145411aa96dd8a986d0f57
Allan <ariosmaia@yahoo.com.br>
2020-04-20 10:23:47 -0300

    Pacotes e visibilidade

---
+++ contas/contaCorrente.go
@@ -0,0 +1,39 @@
+package contas
+
+// Propriedade em minusculo ele fica private
+// Maiusculo ele fica public
+type ContaCorrente struct {
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDosaque float64) string {
+	podeSacar := valorDosaque > 0 && valorDosaque <= c.Saldo
+	if podeSacar {
+		c.Saldo -= valorDosaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Salado insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.Saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.Saldo
+	} else {
+		return "Valor do deposito menor que zero", c.Saldo
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
+		c.Saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
--- main.go
+++ main.go
@@ -1,46 +1,13 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDosaque float64) string {
-	podeSacar := valorDosaque > 0 && valorDosaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDosaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Salado insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Deposito realizado com sucesso", c.saldo
-	} else {
-		return "Valor do deposito menor que zero", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	} else {
-		return false
-	}
-}
+import (
+	"banco/contas"
+	"fmt"
+)
 
 func main() {
-	contaDaSilvia := ContaCorrente{titular: "Silvia", saldo: 300}
-	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 100}
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
 
 	status := contaDaSilvia.Transferir(-200, &contaDoGustavo)
 
@@ -51,8 +18,8 @@ func main() {
 }
 
 // Nessa aula:
-// Criamos um nova conta corrente utilizando a palavra new;
+// Criamos um método chamado depositar, que quando invocado nos retorna uma mensagem (string) e o valor do novo salário (float64);
 
-// Em seguida, comparamos os tipos criados comparando suas referências e entendemos o que são ponteiros na prática;
+// Em seguida, criamos o método transferir, que tira um valor de uma conta e transfere para uma conta destino reutilizando o método depositar;
 
-// Para finalizar, desenvolvemos o método sacar que verifica se o valor do saque é maior do que zero e se a conta possui saldo.
+// Para finalizar, criamos um pacote chamado contas e criamos um arquivo chamado contaCorrente.go para manter todo código referente a este tipo de conta.
