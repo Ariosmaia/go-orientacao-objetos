8b2ae41cab3b419ffcc2bd40fccc01cb79152b70
Allan <ariosmaia@yahoo.com.br>
2020-04-20 09:47:04 -0300

    Criando o método sacar

--- main.go
+++ main.go
@@ -9,26 +9,24 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+// * Para apontar para conta de quem estiver chamando
+func (c *ContaCorrente) Sacar(valorDosaque float64) string {
+	podeSacar := valorDosaque > 0 && valorDosaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDosaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Salado insuficiente"
+	}
+}
+
 func main() {
-	// contaDoAllan := ContaCorrente{titular: "Allan",
-	// 	numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	// contaDaAlice := ContaCorrente{"Alice", 222, 111222, 200}
-	// fmt.Println(contaDoAllan)
-	// fmt.Println(contaDaAlice)
+	fmt.Println(contaDaSilvia.saldo)
 
-	var contaDaCris *ContaCorrente
-	contaDaCris = new(ContaCorrente)
-	contaDaCris.titular = "Cris"
-	contaDaCris.saldo = 500
-
-	var contaDaCris2 *ContaCorrente
-	contaDaCris2 = new(ContaCorrente)
-	contaDaCris2.titular = "Cris"
-	contaDaCris2.saldo = 500
-
-	fmt.Println(&contaDaCris)
-	fmt.Println(&contaDaCris2)
-
-	fmt.Println(*contaDaCris == *contaDaCris2)
+	fmt.Println(contaDaSilvia.Sacar(200))
+	fmt.Println(contaDaSilvia.saldo)
 }
