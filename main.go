e43e1a40dd75fcd8f9a3d177524cbc6fbfdd2b56
Allan <ariosmaia@yahoo.com.br>
2020-04-20 11:08:25 -0300

    Interface anotacao

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
 	conta.Sacar(valorDoBoleto)
 }
 
+// Uma interface é a definição de um conjunto de métodos comuns a um ou mais tipos. É o que permite a criação de tipos polimórficos em Go.
 type verificarConta interface {
 	Sacar(valor float64) string
 }
