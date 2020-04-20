b0520cf97dcf5be5c576b006f4c253af3f252e8d
Allan <ariosmaia@yahoo.com.br>
2020-04-20 10:59:15 -0300

    Conta poupanca

---
+++ contas/contaPoupanca.go
@@ -0,0 +1,32 @@
+package contas
+
+import "banco/clientes"
+
+type ContaPoupanca struct {
+	Titular                              clientes.Titular
+	NumeroAgencia, NumeroConta, Operacao int
+	saldo                                float64
+}
+
+func (c *ContaPoupanca) Sacar(valorDosaque float64) string {
+	podeSacar := valorDosaque > 0 && valorDosaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDosaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Salado insuficiente"
+	}
+}
+
+func (c *ContaPoupanca) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do deposito menor que zero", c.saldo
+	}
+}
+
+func (c *ContaPoupanca) ObterSaldo() float64 {
+	return c.saldo
+}
--- main.go
+++ main.go
@@ -6,15 +6,8 @@ import (
 )
 
 func main() {
-	contaExemplo := contas.ContaCorrente{}
-	contaExemplo.Depositar(100)
+	contaDoDennis := contas.ContaPoupanca{}
+	contaDoDennis.Depositar(100)
 
-	fmt.Println(contaExemplo.ObterSaldo())
+	fmt.Println(contaDoDennis.ObterSaldo())
 }
-
-// Nessa aula:
-// Criamos um novo pacote chamado clientes e um arquivo chamado clientes.go, onde desenvolvemos uma nova struct chamada Titular com nome, CPF e profissão;
-
-// Em seguida, alteramos o campo titular da struct contaCorrente para ser do tipo da struct Titular;
-
-// Para finalizar, alteramos a visibilidade do campo saldo e criamos um método chamado obterSaldo.
