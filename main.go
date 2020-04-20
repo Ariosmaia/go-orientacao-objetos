1de7898608f8908a6141019cb98cd99defe1c4fd
Allan <ariosmaia@yahoo.com.br>
2020-04-20 10:49:47 -0300

    Alterando a visibilidade

--- main.go
+++ main.go
@@ -11,3 +11,10 @@ func main() {
 
 	fmt.Println(contaExemplo.ObterSaldo())
 }
+
+// Nessa aula:
+// Criamos um novo pacote chamado clientes e um arquivo chamado clientes.go, onde desenvolvemos uma nova struct chamada Titular com nome, CPF e profissão;
+
+// Em seguida, alteramos o campo titular da struct contaCorrente para ser do tipo da struct Titular;
+
+// Para finalizar, alteramos a visibilidade do campo saldo e criamos um método chamado obterSaldo.
